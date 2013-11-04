733243d3e021d5a9a03b109a681ecbf382bcccf0
David <david@MacBook-Pro-de-david-2.local>
2013-11-04 18:03:28 +0100

    maj modele

---
+++ tables/MembreManager.class.php
@@ -0,0 +1,66 @@
+<?php
+//exemple de table Membre
+/*
+//structure SQL : 
+CREATE TABLE `Membres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `mail` varchar(40) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+*/
+
+
+
+//classe de gestion des entités Membre
+class MembreManager{
+	
+
+		public  function creer($m){
+			
+			$sql = "INSERT INTO Membre VALUES ('',?, ?, ?, ?, ?)";
+			$res = DB::get_instance()->prepare($sql);
+			$res -> execute(array($m->login, $m->nom, $m->prenom,$m->mail,$m->pass));
+			$m->id=DB::get_instance()->lastInsertId();
+			return $m;
+			
+		}
+
+
+		public  function chercherParID($id){
+			$sql="SELECT * from Membre WHERE id=?";
+			$res=DB::get_instance()->prepare($sql);
+			$res->execute(array($id));
+			//gérer les erreurs éventuelles
+			if($res->rowCount()==0){
+				return false;
+			}
+			$m= $res->fetch();			
+			$membre=new Membre();
+			$membre->id=$m[0];
+			$membre->login=$m[1];
+			$membre->nom= $m[2];
+			$membre->prenom=$m[3];
+			$membre->mail=$m[4];
+			$membre->pass=$m[5];											
+			return $membre;
+		}
+		
+		
+		//autres exemples de fonctions possibles
+		public function liste(){
+			
+		}   		
+
+		public function desactiver(){
+			
+		}
+		public function activer(){
+			
+		}
+}
+
+?>
