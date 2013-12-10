c5456af9441bb9ed6ccba41db0ea8352f6c32970
David <david@MacBook-Pro-de-david-2.local>
2013-12-10 18:17:13 +0100

    Ajout exemple CRUD

--- conf/Menus.ini.php
+++ conf/Menus.ini.php
@@ -1,8 +1,8 @@
 <?php
-
 $menus['Exemples']=array(
 	'SimpleTPL'=>"?module=SimpleTemplate",
 	'Formulaire'=>"?module=Formulaire",
+	'C.R.U.D'=>"?module=CRUD",	
 	'Redirect'=>"?module=Redirect",
 	'Download'=>"?module=DownloadFile",
 	'Ajax'=>"?module=Ajax"
