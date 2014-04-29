ca68943514fb17580d415c2ac49f5358e9d012c0
David <david@macbook.home>
2014-04-29 19:38:20 +0200

    js bootstrap

---
+++ js/default.js
@@ -0,0 +1,16 @@
+
+$(function(){
+	//surbrille les boutons faisant office de radio (bootstrap)
+	$(function(){$('input[type="radio"]:checked').parent('label').addClass('active');})
+
+
+
+
+
+
+
+
+
+
+
+})
